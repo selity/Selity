b072e6be0ce85fc118999400015f20a2ad86c83f
Daniel Andreca <sci2tech@gmail.com>
2015-05-03 19:52:20 +0300

    Small fix

--- gui/public/include/selity-functions.php
+++ gui/public/include/selity-functions.php
@@ -19,17 +19,7 @@
  */
 
 function check_for_lock_file($wait_lock_timeout = 500000) {
-
-	set_time_limit(0);
-	// @ prevents the Warning:
-	// File(/var/log/chkrootkit.log) is not within the allowed path(s)
-	while(@file_exists(Config::get('MR_LOCK_FILE'))) {
-
-		usleep($wait_lock_timeout);
-		clearstatcache();
-		// and send header to keep connection
-		header( "Cache-Control: no-store, no-cache, must-revalidate" );
-	}
+	return;
 }
 
 function read_line(&$socket) {
